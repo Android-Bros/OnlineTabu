7d1b489754f62ba9494ecaddd0c5b9d4a97777e6
adematici <adematicis41@gmail.com>
2021-12-20 02:19:24 +0300

    setup crashlytics

--- app/src/main/java/com/androidbros/onlinetabu/MainActivity.kt
+++ app/src/main/java/com/androidbros/onlinetabu/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.androidbros.onlinetabu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androidbros.onlinetabu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            throw RuntimeException("Test Crash")
+        }
     }
+
 }
