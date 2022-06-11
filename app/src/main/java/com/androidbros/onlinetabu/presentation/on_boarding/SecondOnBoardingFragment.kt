f6bea682ace3ef1d0b201960a07c8653e219e784
Adem <adematicis41@gmail.com>
2022-06-11 13:36:11 +0300

    Update on boarding screens logic

--- app/src/main/java/com/androidbros/onlinetabu/presentation/on_boarding/SecondOnBoardingFragment.kt
+++ app/src/main/java/com/androidbros/onlinetabu/presentation/on_boarding/SecondOnBoardingFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.androidbros.onlinetabu.R
 import com.androidbros.onlinetabu.databinding.FragmentSecondOnBoardingBinding
 
 class SecondOnBoardingFragment : Fragment() {
@@ -18,6 +20,16 @@ class SecondOnBoardingFragment : Fragment() {
     ): View {
         _binding = FragmentSecondOnBoardingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+
+        binding.btnSecondOnBoardingForward.setOnClickListener{
+            viewPager?.currentItem = 2
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/androidbros/onlinetabu/presentation/on_boarding/ThirdOnBoardingFragment.kt
+++ app/src/main/java/com/androidbros/onlinetabu/presentation/on_boarding/ThirdOnBoardingFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.androidbros.onlinetabu.R
 import com.androidbros.onlinetabu.databinding.FragmentThirdOnBoardingBinding
+import com.androidbros.onlinetabu.util.SharedPref
 
 class ThirdOnBoardingFragment : Fragment() {
 
     private var _binding: FragmentThirdOnBoardingBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mySharedPref: SharedPref
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +24,25 @@ class ThirdOnBoardingFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mySharedPref = SharedPref(requireContext())
+
+        binding.btnThirdOnBoardingFinish.setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_welcomeScreenFragment)
+            isOnBoardingFinish()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // onBoarding ekranlarinin tekrar gosterilmemesi icin fonksiyon
+    private fun isOnBoardingFinish() {
+        mySharedPref.setOnBoardingState(true)
     }
 
 }
